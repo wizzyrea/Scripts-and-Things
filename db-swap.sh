105614a791cb0ec98c5794ef0580749bcdb4e313
Liz Rea - Catalyst <liz@catalyst.net.nz>
2012-11-20 09:01:39 +1300

    Added ability to switch git branches + databases at the same time.
    Could add more options for other branches + databases. Later.

--- db-swap.sh
+++ db-swap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-confpath=/home/liz/koha-dev/etc
-kohapath=/home/liz/kohaclone
+confpath=/home/koha/koha-dev/etc
+kohapath=/home/koha/kohaclone
 export KOHA_CONF=$confpath/koha-conf.xml
 export PERL5LIB=$kohapath
 
@@ -15,23 +15,26 @@ do
       m)  mode="$OPTARG";;
       \?)		# unknown flag
       	  echo >&2 \
-	  "usage: $0 [-v] [-m o || t] o is original config, t is testing config."
+	  "usage: $0 [-v] [-m o || t] 36 is 3.6, 38 is 3.8."
 	  exit 1;;
     esac
 done
 shift `expr $OPTIND - 1`
 
-if [ $mode = o ]
+if [ $mode = 36 ]
 then
-	echo "copying in original config"
-	cp -f $confpath/koha-conf.orig $confpath/koha-conf.xml
-elif [ $mode = t ]
+	echo "copying in 3.6 config"
+	cp -f $confpath/koha-conf.36 $confpath/koha-conf.xml
+	git checkout 3.6.x
+elif [ $mode = 38 ]
 then
-		echo "copying in test config" 
-		cp -f $confpath/koha-conf.test $confpath/koha-conf.xml
+	echo "copying in 3.8 config" 
+	cp -f $confpath/koha-conf.38 $confpath/koha-conf.xml
+	git checkout 3.8.x
 else
-	echo "please specify -m o for original configuration or -m t for testing configuration"
+	echo "please specify -m 36 for 3.6 configuration or -m 38 for 3.8 configuration"
 fi
 echo "reindexing on the new DB..."
 
 perl $kohapath/misc/migration_tools/rebuild_zebra.pl -b -r -v
+echo "System configured for version $mode"
