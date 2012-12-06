5c407237702b2018508953363ec00a38e9b8de36
Liz Rea <liz@catalyst.net.nz>
2012-12-07 11:55:19 +1300

    decrypt the sudo password - clear the screen after

--- getpassword.sh
+++ getpassword.sh
@@ -1,6 +1,10 @@
+gpg -d ~/password.txt.gpg
 ls /etc/koha/sites
 echo "Which site?"
 read site
 
 sudo xmlstarlet sel -t -v 'yazgfs/config/pass' /etc/koha/sites/$site/koha-conf.xml
 
+echo "Press any key to clear the screen..."
+read waitforme
+clear
